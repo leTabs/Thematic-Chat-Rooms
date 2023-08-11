a3eb415c830b96e765209270bb765cfa2a38044f
leTabs <125308756+leTabs@users.noreply.github.com>
2023-08-11 22:55:17 +0300

    Update chat_themes.py

--- chat_themes.py
+++ chat_themes.py
@@ -1,7 +1,11 @@
+# the dictionary stores the available themes.
+# special treatment isn't required other than adding or removing items from "themes"
 themes = {
     1:'Music',
     2:'Art',
     3:'Movies',
     4:'Comics',
     5:'Programming',
+    6:'Working Out',
+    7:'Travel'
 }
